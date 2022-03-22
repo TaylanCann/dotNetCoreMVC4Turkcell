d3205741e249e6f8350484d630c5981e85d35adf
TaylanCann <taylancanh@gmail.com>
2022-03-23 00:27:54 +0300

    m

--- dotNetCoreMVC4Turkcell/Startup.cs
+++ dotNetCoreMVC4Turkcell/Startup.cs
@@ -22,6 +22,7 @@ namespace dotNetCoreMVC4Turkcell
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -33,7 +34,7 @@ namespace dotNetCoreMVC4Turkcell
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Uygulama calistiiiiii!");
                 });
             });
         }
